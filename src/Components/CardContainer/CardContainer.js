937dca1dab24ceed68c4565c0e9887249b139f26
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-08-23 13:23:02 -0600

    Fix CardContainer tests

--- src/Components/CardContainer/CardContainer.js
+++ src/Components/CardContainer/CardContainer.js
@@ -4,9 +4,9 @@ import './CardContainer.css';
 import Card from '../Card/Card'
 
 const CardContainer = ({ category, toggleFavorites, favorites }) => {
-  const displayedCards = category.map( facts => {
+  const displayedCards = category.map( (facts, index) => {
     const selected = favorites.includes(facts)
-    return <Card facts={facts} toggleFavorites={toggleFavorites} selected={selected} />
+    return <Card key={category.name + index} facts={facts} toggleFavorites={toggleFavorites} selected={selected} />
   });
 
   return (
--- src/Components/CardContainer/CardContainer.test.js
+++ src/Components/CardContainer/CardContainer.test.js
@@ -4,47 +4,29 @@ import { shallow } from 'enzyme';
 import CardContainer from './CardContainer';
 
 describe('CardContainer', () => {
-
-  let wrapper;
-  let category;
-  let favorites;
-  let selected;
-
-  beforeEach(() => {
-    category = [];
-    favorites = [];
-    selected = false;
-    wrapper = shallow(<CardContainer category={category} favorites={favorites}/>);
-  })
-
+  
   it('should match the snapshot', () => {
-    expect(wrapper).toMatchSnapshot();
+    const category = [];
+    const favorites = [];
+    const wrapper = shallow(<CardContainer category={category} favorites={favorites}/>);
+    
+    expect(wrapper.html()).toMatchSnapshot();
   })
 
   it('should match the snapshot when one card is in category array', () => {
-    category = [{name: 'test', origin: 'the matrix'}];
-    
-    expect(wrapper).toMatchSnapshot();  
+    const category = [{name: 'test', origin: 'the matrix'}];
+    const favorites = [];
+    const wrapper = shallow(<CardContainer category={category} favorites={favorites}/>);
+
+    expect(wrapper.html()).toMatchSnapshot();  
   })
 
   it('should match the snapshot when multiple cards are in category array', () => {
-    category = [{name: 'test1', origin: 'the matrix'}, {name: 'test2', origin: 'the matrix'}];
-    
-    expect(wrapper).toMatchSnapshot();  
-  })
+    const category = [{name: 'test1', origin: 'the matrix'}, {name: 'test2', origin: 'the matrix'}];
+    const favorites = [];
+    const wrapper = shallow(<CardContainer category={category} favorites={favorites}/>);
 
-  it.skip('should set the value of selected based on if favorites includes the card facts', () => {
-    category = [{name: 'test', origin: 'the matrix'}];
-    favorites = [];
-
-    expect(selected).toEqual(false);
-
-    category = [{name: 'test', origin: 'the matrix'}];
-    favorites = [{name: 'test', origin: 'the matrix'}];
-
-    // wrapper.instance()
-
-    expect(selected).toEqual(true);
+    expect(wrapper.html()).toMatchSnapshot();  
   })
 
 })
