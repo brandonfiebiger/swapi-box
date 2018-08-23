242d39530323c84da125e9b3596910b0ac318e63
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-08-23 13:27:05 -0600

    Remove wrapper from beforeEach in Card test

--- src/Components/Card/Card.test.js
+++ src/Components/Card/Card.test.js
@@ -5,7 +5,6 @@ import Card from './Card';
 
 describe('Card', () => {
 
-  let wrapper;
   let facts;
   let mockToggleFavorites;
   let selected;
@@ -14,19 +13,19 @@ describe('Card', () => {
     facts = {};
     mockToggleFavorites = jest.fn();
     selected = false;
-
-    wrapper = shallow(<Card facts={facts} 
-      toggleFavorites={mockToggleFavorites} 
-      selected={selected} />);
   })
 
   it('should match the snapshot', () => {
+    const wrapper = shallow(<Card facts={facts} 
+      toggleFavorites={mockToggleFavorites} 
+      selected={selected} />);
+
     expect(wrapper.html()).toMatchSnapshot();
   })
 
   it('should match the snapshot with a fact', () => {
     facts = {name: 'test'};
-    wrapper = shallow(<Card facts={facts} 
+    const wrapper = shallow(<Card facts={facts} 
       toggleFavorites={mockToggleFavorites} 
       selected={selected} />);
 
@@ -35,8 +34,7 @@ describe('Card', () => {
 
   it('should match the snapshot with multiple facts', () => {
     facts = {name: 'test1', origin: 'the matrix'};
-    
-    wrapper = shallow(<Card facts={facts} 
+    const wrapper = shallow(<Card facts={facts} 
       toggleFavorites={mockToggleFavorites} 
       selected={selected} />);
 
@@ -44,12 +42,20 @@ describe('Card', () => {
   })
 
   it('should call toggleFavorites with the correct parameters when clicked', () => {
+    const wrapper = shallow(<Card facts={facts} 
+      toggleFavorites={mockToggleFavorites} 
+      selected={selected} />);
+    
     wrapper.find('.Card').simulate('click');
 
     expect(mockToggleFavorites).toHaveBeenCalledWith(facts);
   })
 
   it('should change color of svg depending on if selected is true or false', () => {
+    let wrapper = shallow(<Card facts={facts} 
+      toggleFavorites={mockToggleFavorites} 
+      selected={selected} />);
+    
     expect(wrapper.find('path').prop('fill')).toEqual('#f2f2f2');
 
     selected = true;
