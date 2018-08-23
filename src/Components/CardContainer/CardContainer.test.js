ea9b670cd656c24ed47308cda4c72c3843933a9d
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-08-23 08:43:55 -0600

    Add CardContainer tests

--- src/Components/CardContainer/CardContainer.test.js
+++ src/Components/CardContainer/CardContainer.test.js
@@ -1,29 +1,50 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 
 import CardContainer from './CardContainer';
 
 describe('CardContainer', () => {
 
   let wrapper;
-  let infoCards;
+  let category;
+  let favorites;
+  let selected;
+
   beforeEach(() => {
-    infoCards = [];
-    wrapper = shallow(<CardContainer infoCards={infoCards}/>);
+    category = [];
+    favorites = [];
+    selected = false;
+    wrapper = shallow(<CardContainer category={category} favorites={favorites}/>);
   })
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   })
 
-  it('should match the snapshot when one card is in infoCards array', () => {
-    infoCards = [{}];
+  it('should match the snapshot when one card is in category array', () => {
+    category = [{name: 'test', origin: 'the matrix'}];
+    
     expect(wrapper).toMatchSnapshot();  
   })
 
-  it('should match the snapshot when multiple cards are in infoCards array', () => {
-    infoCards = [{}, {}];
+  it('should match the snapshot when multiple cards are in category array', () => {
+    category = [{name: 'test1', origin: 'the matrix'}, {name: 'test2', origin: 'the matrix'}];
+    
     expect(wrapper).toMatchSnapshot();  
   })
+
+  it.skip('should set the value of selected based on if favorites includes the card facts', () => {
+    category = [{name: 'test', origin: 'the matrix'}];
+    favorites = [];
+
+    expect(selected).toEqual(false);
+
+    category = [{name: 'test', origin: 'the matrix'}];
+    favorites = [{name: 'test', origin: 'the matrix'}];
+
+    wrapper.instance()
+
+    expect(selected).toEqual(true);
+  })
+
 })
