8dca562c007e985c5cabb51c69abf6dcefd964f6
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-08-23 08:13:31 -0600

    Add Card tests

--- src/Components/Card/Card.test.js
+++ src/Components/Card/Card.test.js
@@ -1,18 +1,54 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 
 import Card from './Card';
 
 describe('Card', () => {
 
   let wrapper;
+  let facts;
+  let mockToggleFavorites;
+  let selected;
 
   beforeEach(() => {
-    wrapper = shallow(<Card />);
+    facts = [];
+    mockToggleFavorites = jest.fn();
+    selected = false;
+
+    wrapper = shallow(<Card facts={facts} 
+      toggleFavorites={mockToggleFavorites} 
+      selected={selected} />);
   })
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should match the snapshot with a fact', () => {
+    facts = [{name: 'test', origin: 'the matrix'}];
+
+    expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should match the snapshot with multiple facts', () => {
+    facts = [{name: 'test1', origin: 'the matrix'}, {name: 'test2', origin: 'the matrix'}];
+ 
+    expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should call toggleFavorites with the correct parameters when clicked', () => {
+    wrapper.find('.Card').simulate('click');
+
+    expect(mockToggleFavorites).toHaveBeenCalledWith(facts);
+  })
+
+  it('should change color of svg depending on if selected is true or false', () => {
+    expect(wrapper.find('path').props('fill')).toEqual('#f2f2f2');
+
+    selected = true;
+    wrapper.instance().forceUpdate();
+
+    expect(wrapper.find('path').props('fill')).toEqual('#fac917');
+
   })
 })
