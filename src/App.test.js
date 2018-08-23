6c4f727d03f4268357148f1a34ebf5da9475bc82
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-08-22 19:30:42 -0600

    Add first test for fetch data method in app

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow, mount } from 'enzyme';
+import mockFilmResult from './mockFilmResult'
 
 import App from './App';
 
@@ -15,4 +16,15 @@ describe('App', () => {
   it('should match snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+  describe('fetchData', () => {
+    it('should set state to the appropiate category if fetch works', async () => {
+      await wrapper.instance().fetchData('films')
+      expect(wrapper.state('films')).toEqual(mockFilmResult)
+    })
+
+    it('should throw an error if the fetch doesnt work', async () => {
+      
+    })  
+  })
 })
+
