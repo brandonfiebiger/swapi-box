acf7584aa04fd646751f1f7e2f1c9b7f78310b0c
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-08-22 19:58:26 -0600

    Add tests for select category method

--- src/App.test.js
+++ src/App.test.js
@@ -23,8 +23,30 @@ describe('App', () => {
     })
 
     it('should throw an error if the fetch doesnt work', async () => {
+      await wrapper.instance().fetchData('nope')
+      expect(wrapper.state('error')).toEqual(true)
+    })
+    
+    // it('should call CleanData method', async () => {
+    //   await wrapper.instance().fetchData('films')
       
-    })  
+    // })
+  })
+
+  describe('selectCategory', () => {
+    it('should set the state of loading to true if the selected categories array does not have length',  () => {
+      wrapper.instance().selectCategory('planets');
+      expect(wrapper.state('loading')).toEqual(true);
+    })
+
+    it('should set the state of selectedCategory to the selected category', () => {
+      wrapper.instance().selectCategory('planets');
+      expect(wrapper.state('selectedCategory')).toEqual('planets');
+    })
+
+    // it('should call the fetchData method', () => {
+    //   wrapper.instance().selectCategory('planets')
+    // })
   })
 })
 
