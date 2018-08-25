a0a1ec0161d27995f7a73e520045af4ba5670d9f
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-08-25 12:57:10 -0600

    Add proptypes, scss, fix eslint errors for Card and CardContainer

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import './Card.css';
 
 const Card = ({ facts, toggleFavorites, selected }) => {
-  let fillColor = '#f2f2f2';
-  const cardFacts = Object.keys(facts).map( (fact, i) => {
-    selected ? fillColor = '#fac917' : fillColor;
-    return <li key={i} >{fact}: {facts[fact]}</li>
+  let fillColor;
+  const cardFacts = Object.keys(facts).map( (fact, index) => {
+    selected ? fillColor = '#fac917' : fillColor = '#f2f2f2';
+    return <li key={index}>{fact}: {facts[fact]}</li>;
   });
 
   return (
@@ -17,14 +19,23 @@ const Card = ({ facts, toggleFavorites, selected }) => {
         enableBackground="new 0 0 426.667 426.667" 
         viewBox="0 0 426.667 426.667" >
         <path 
-          d="m213.333 10.441 65.916 133.576 147.418 21.419-106.667 103.974 25.173 146.816-131.84-69.316-131.848 69.316 25.182-146.816-106.667-103.974 147.409-21.419z" 
+          d="m213.333 10.441 65.916 133.576 147.418 
+          21.419-106.667 103.974 25.173 
+          146.816-131.84-69.316-131.848 69.316 
+          25.182-146.816-106.667-103.974 147.409-21.419z" 
           fill={fillColor}/>
       </svg>
       <ul>
         {cardFacts}
       </ul>
     </div>
-  )
-}
+  );
+};
 
 export default Card;
+
+Card.propTypes = {
+  facts: PropTypes.objectOf(PropTypes.string),
+  toggleFavorites: PropTypes.func,
+  selected: PropTypes.bool
+};
--- src/Components/CardContainer/CardContainer.js
+++ src/Components/CardContainer/CardContainer.js
@@ -1,19 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import './CardContainer.css';
 
-import Card from '../Card/Card'
+import Card from '../Card/Card';
 
-const CardContainer = ({ category, toggleFavorites, favorites }) => {
+const CardContainer = ({ 
+  category, 
+  toggleFavorites, 
+  favorites 
+}) => {
   const displayedCards = category.map( (facts, index) => {
-    const selected = favorites.includes(facts)
-    return <Card key={category.name + index} facts={facts} toggleFavorites={toggleFavorites} selected={selected} />
+    const selected = favorites.includes(facts);
+    return <Card 
+      key={index} 
+      facts={facts} 
+      toggleFavorites={toggleFavorites} 
+      selected={selected} />;
   });
 
   return (
     <section>
       {displayedCards}
     </section>
-)
-}
+  );
+};
 
 export default CardContainer;
+
+CardContainer.propTypes = {
+  category: PropTypes.arrayOf(PropTypes.object),
+  toggleFavorites: PropTypes.func,
+  favorites: PropTypes.arrayOf(PropTypes.object)
+};
