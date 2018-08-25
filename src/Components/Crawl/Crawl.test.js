2aa886db5d04f4e2b03b9efaa5960563de8cb290
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-08-25 12:30:10 -0600

    Fix eslint errors on Crawl

--- src/Components/Crawl/Crawl.test.js
+++ src/Components/Crawl/Crawl.test.js
@@ -6,21 +6,24 @@ import Crawl from './Crawl';
 describe('Crawl', () => {
   
   it('should match snapshot', () => {
-    const films = []
+    const films = [];
     const wrapper = shallow(<Crawl films={films}/>);
     
     expect(wrapper.html()).toMatchSnapshot();
   });
 
   it('should match snapshot with one film', () => {
-    const films = [{ title: 'test1', crawl: 'text', date: '1'}]
+    const films = [{ title: 'test1', crawl: 'text', date: '1'}];
     const wrapper = shallow(<Crawl films={films}/>);
     
     expect(wrapper.html()).toMatchSnapshot();
   });
 
   it('should match snapshot with multiple films', () => {
-    const films = [{ title: 'test1', crawl: 'text', date: '1' }, { title: 'test2', crawl: 'text', date: '2' }]
+    const films = [
+      { title: 'test1', crawl: 'text', date: '1' }, 
+      { title: 'test2', crawl: 'text', date: '2' }
+    ];
     const wrapper = shallow(<Crawl films={films}/>);
     
     expect(wrapper.html()).toMatchSnapshot();
